8b48303b12189905409e47a0d25f4cd7d07039e8
Antonella Lusardi <69723365+AntoCLus@users.noreply.github.com>
2022-11-15 09:12:29 +0100

    Add files via upload

---
+++ practiceWithTuples.py
@@ -0,0 +1,8 @@
+my_tuple = ("anto", "teacher", "argentinean", "married")
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple[1:])
+print(my_tuple[:-2])
+
+for elem in my_tuple:
+    print(elem)
